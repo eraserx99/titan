4674c86bae57fe83e35e7ca067c304c5e81f14a9
Will Wilson <wilson.william@gmail.com>
2013-05-09 12:13:24 -0400

    Create wrapper object for FoundationDB transactions.

---
+++ titan-foundationdb/src/main/java/com/thinkaurelius/titan/diskstorage/foundationdb/FoundationDBTransaction.java
@@ -0,0 +1,40 @@
+package com.thinkaurelius.titan.diskstorage.foundationdb;
+
+import com.foundationdb.FDBError;
+import com.foundationdb.Transaction;
+import com.thinkaurelius.titan.diskstorage.PermanentStorageException;
+import com.thinkaurelius.titan.diskstorage.StorageException;
+import com.thinkaurelius.titan.diskstorage.TransactionHandle;
+
+public class FoundationDBTransaction implements TransactionHandle {
+
+    private Transaction tr;
+
+    public FoundationDBTransaction(Transaction tr) {
+        this.tr = tr;
+    }
+
+    @Override
+    public void commit() throws StorageException {
+        if (tr == null) return;
+        try {
+            tr.commit().get();
+            tr = null;
+        }
+        catch(FDBError err) {
+            throw new PermanentStorageException(err.getMessage(),err.getCause());
+        }
+    }
+
+    @Override
+    public void rollback() throws StorageException {
+        if (tr == null) return;
+        tr.reset();
+        tr = null;
+    }
+
+    @Override
+    public void flush() throws StorageException {
+
+    }
+}
