68178d39b333dd131454b112f1e80684d64457e3
Will Wilson <wilson.william@gmail.com>
2013-05-09 15:50:10 -0400

    Unimplemented version of the store.

--- titan-foundationdb/src/main/java/com/thinkaurelius/titan/diskstorage/foundationdb/FoundationDBKeyColumnValueStore.java
+++ titan-foundationdb/src/main/java/com/thinkaurelius/titan/diskstorage/foundationdb/FoundationDBKeyColumnValueStore.java
@@ -1,4 +1,66 @@
 package com.thinkaurelius.titan.diskstorage.foundationdb;
 
-public class FoundationDBKeyColumnValueStore {
+import com.thinkaurelius.titan.diskstorage.StorageException;
+import com.thinkaurelius.titan.diskstorage.keycolumnvalue.*;
+
+import java.nio.ByteBuffer;
+import java.util.List;
+
+public class FoundationDBKeyColumnValueStore implements KeyColumnValueStore {
+
+    private final String storeName;
+
+    public FoundationDBKeyColumnValueStore(String name) {
+        storeName = name;
+    }
+
+    @Override
+    public boolean containsKey(ByteBuffer key, StoreTransaction txh) throws StorageException {
+        return false;  // todo
+    }
+
+    @Override
+    public List<Entry> getSlice(KeySliceQuery query, StoreTransaction txh) throws StorageException {
+        return null;  // todo
+    }
+
+    @Override
+    public ByteBuffer get(ByteBuffer key, ByteBuffer column, StoreTransaction txh) throws StorageException {
+        return null;  // todo
+    }
+
+    @Override
+    public boolean containsKeyColumn(ByteBuffer key, ByteBuffer column, StoreTransaction txh) throws StorageException {
+        return false;  // todo
+    }
+
+    @Override
+    public void mutate(ByteBuffer key, List<Entry> additions, List<ByteBuffer> deletions, StoreTransaction txh) throws StorageException {
+        // todo
+    }
+
+    @Override
+    public void acquireLock(ByteBuffer key, ByteBuffer column, ByteBuffer expectedValue, StoreTransaction txh) throws StorageException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public RecordIterator<ByteBuffer> getKeys(StoreTransaction txh) throws StorageException {
+        return null;  // todo
+    }
+
+    @Override
+    public ByteBuffer[] getLocalKeyPartition() throws StorageException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String getName() {
+        return storeName;
+    }
+
+    @Override
+    public void close() throws StorageException {
+        // todo
+    }
 }
