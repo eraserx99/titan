e92479ca427a626a6255830d032fe7cb3b25aada
Will Wilson <wilson.william@gmail.com>
2013-05-10 13:31:09 -0400

    Clean up the way we do subspaces a little.

--- titan-foundationdb/src/main/java/com/thinkaurelius/titan/diskstorage/foundationdb/FoundationDBKeyColumnValueStore.java
+++ titan-foundationdb/src/main/java/com/thinkaurelius/titan/diskstorage/foundationdb/FoundationDBKeyColumnValueStore.java
@@ -17,7 +17,17 @@ public class FoundationDBKeyColumnValueStore implements KeyColumnValueStore {
     private final String storeName;
     private final FoundationDBStoreManager manager;
 
-    private static final int KEYS_SUBSPACE = 1;
+    private static enum Subspace {
+
+        KEYS_SUBSPACE(1),
+        DATA_SUBSPACE(2);
+
+        private Subspace(int value) {
+            this.value = value;
+        }
+
+        public final int value;
+     }
 
     public FoundationDBKeyColumnValueStore(String name, FoundationDBStoreManager m) {
         dbName = m.dbname;
@@ -28,13 +38,13 @@ public class FoundationDBKeyColumnValueStore implements KeyColumnValueStore {
     @Override
     public boolean containsKey(ByteBuffer key, StoreTransaction txh) throws StorageException {
         FoundationDBTransaction tr = (FoundationDBTransaction) txh;
-        return tr.get(storePrefix().add(KEYS_SUBSPACE).add(key.array()).pack()) != null;
+        return tr.get(storePrefix(Subspace.KEYS_SUBSPACE).add(key.array()).pack()) != null;
     }
 
     @Override
     public List<Entry> getSlice(KeySliceQuery query, StoreTransaction txh) throws StorageException {
         List<Entry> returnList = new ArrayList<Entry>();
-        RangeQuery queryResult = ((FoundationDBTransaction) txh).getRange(storePrefix().add(query.getKey().array()).add(query.getSliceStart().array()).pack(), storePrefix().add(query.getKey().array()).add(query.getSliceEnd().array()).pack()).limit(query.getLimit());
+        RangeQuery queryResult = ((FoundationDBTransaction) txh).getRange(storePrefix(Subspace.DATA_SUBSPACE).add(query.getKey().array()).add(query.getSliceStart().array()).pack(), storePrefix(Subspace.DATA_SUBSPACE).add(query.getKey().array()).add(query.getSliceEnd().array()).pack()).limit(query.getLimit());
         List<KeyValue> kvList = queryResult.asList().get();
         assert kvList.size() < query.getLimit();
 
@@ -48,27 +58,27 @@ public class FoundationDBKeyColumnValueStore implements KeyColumnValueStore {
     @Override
     public ByteBuffer get(ByteBuffer key, ByteBuffer column, StoreTransaction txh) throws StorageException {
         FoundationDBTransaction tr = (FoundationDBTransaction) txh;
-        return ByteBuffer.wrap(tr.get(storePrefix().add(key.array()).add(column.array()).pack()));
+        return ByteBuffer.wrap(tr.get(storePrefix(Subspace.DATA_SUBSPACE).add(key.array()).add(column.array()).pack()));
     }
 
     @Override
     public boolean containsKeyColumn(ByteBuffer key, ByteBuffer column, StoreTransaction txh) throws StorageException {
         FoundationDBTransaction tr = (FoundationDBTransaction) txh;
-        return tr.get(storePrefix().add(key.array()).add(column.array()).pack()) != null;
+        return tr.get(storePrefix(Subspace.DATA_SUBSPACE).add(key.array()).add(column.array()).pack()) != null;
     }
 
     @Override
     public void mutate(ByteBuffer key, List<Entry> additions, List<ByteBuffer> deletions, StoreTransaction txh) throws StorageException {
         if (deletions != null) {
             for (ByteBuffer deleteColumn : deletions) {
-                ((FoundationDBTransaction) txh).clear(storePrefix().add(key.array()).add(deleteColumn.array()).pack());
-                ((FoundationDBTransaction) txh).clear(storePrefix().add(KEYS_SUBSPACE).add(key.array()).pack());
+                ((FoundationDBTransaction) txh).clear(storePrefix(Subspace.DATA_SUBSPACE).add(key.array()).add(deleteColumn.array()).pack());
+                ((FoundationDBTransaction) txh).clear(storePrefix(Subspace.KEYS_SUBSPACE).add(key.array()).pack());
             }
         }
         if (additions != null) {
             for (Entry addColumn : additions) {
-                ((FoundationDBTransaction) txh).set(storePrefix().add(key.array()).add(addColumn.getColumn().array()).pack(), addColumn.getValue().array());
-                ((FoundationDBTransaction) txh).set(storePrefix().add(KEYS_SUBSPACE).add(key.array()).pack(), "".getBytes());
+                ((FoundationDBTransaction) txh).set(storePrefix(Subspace.DATA_SUBSPACE).add(key.array()).add(addColumn.getColumn().array()).pack(), addColumn.getValue().array());
+                ((FoundationDBTransaction) txh).set(storePrefix(Subspace.KEYS_SUBSPACE).add(key.array()).pack(), "".getBytes());
             }
         }
     }
@@ -81,7 +91,7 @@ public class FoundationDBKeyColumnValueStore implements KeyColumnValueStore {
     @Override
     public RecordIterator<ByteBuffer> getKeys(final StoreTransaction txh) throws StorageException {
         return new RecordIterator<ByteBuffer>() {
-            private final Iterator<KeyValue> results = ((FoundationDBTransaction) txh).getRangeStartsWith(storePrefix().add(KEYS_SUBSPACE).pack()).iterator();
+            private final Iterator<KeyValue> results = ((FoundationDBTransaction) txh).getRangeStartsWith(storePrefix(Subspace.KEYS_SUBSPACE).pack()).iterator();
 
             @Override
             public boolean hasNext() throws StorageException {
@@ -115,7 +125,7 @@ public class FoundationDBKeyColumnValueStore implements KeyColumnValueStore {
         // todo
     }
 
-    private Tuple storePrefix() {
-        return new Tuple().add(dbName).add(storeName);
+    private Tuple storePrefix(Subspace s) {
+        return new Tuple().add(dbName).add(storeName).add(s.value);
     }
 }
